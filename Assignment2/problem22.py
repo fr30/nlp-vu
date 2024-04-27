998a0a9541312cc64d3e8482a50405e857c412f2
Arturo Abril Martinez <arturoabrilm@gmail.com>
2024-04-27 13:29:19 +0200

    kk

--- Assignment2/problem22.py
+++ Assignment2/problem22.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
+
 """
+
 NLP A2: N-Gram Language Models
+
+
 
 @author: Klinton Bicknell, Harry Eldridge, Nathan Schneider, Lucia Donatelli, Alexander Koller
 
+
+
 DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
+
 """
+
 
 import numpy as np
 
-from generate import GENERATE
 from problem1 import return_word_index_dict
 
 
